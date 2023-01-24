738934842e1e68b915f0a04b17a4363b585673a1
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-01-24 16:38:42 -0300

    Add files via upload

---
+++ Página de Cadastro/Página de Cadastro/JS/javascripit.js
@@ -0,0 +1,53 @@
+let tagFormularioCadastroUsuario = document.querySelector("#formularioCadastroUsuario")
+tagFormularioCadastroUsuario.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    let nome = event.target.nome.value
+    let email = event.target.email.value
+
+    
+    let formValido = true
+if (nome.trim() == "") {
+
+    document.querySelector("#campoObrigatorio").style.display = "inline"
+    formValido = false
+
+} else  {
+    document.querySelector("#campoObrigatorio").style.display = "none"
+}
+
+if (formValido) {
+
+
+    fetch("https://63442914dcae733e8fd8e3e5.mockapi.io/usuario", {
+	  method: 'POST',
+	  headers: {
+	    'Content-Type': 'application/json',
+	  },
+	  body: JSON.stringify({
+	      nome: nome,
+        email: email
+	  })
+	})
+  .then(function(response) {
+      if (!response.ok) {
+          throw "Requisição chegou no servidor, mas servidor retornou com erro: " + response.statusText
+      }
+      return response.json()
+  })
+  .then(function(usuario) {
+      console.log(usuario)
+      let tagMensagensDeErro =  document.querySelector("#mensagensDeErro")
+      tagMensagensDeErro.textContent = "" 
+      tagMensagensDeErro.style.display = "none"
+      event.target.reset
+
+  })
+  .catch(function(error) {
+      console.log(error)
+      let tagMensagensDeErro =  document.querySelector("#mensagensDeErro")
+      tagMensagensDeErro.textContent = "Ocorreu um erro na requisição do cadastros. Erro:" + error
+      tagMensagensDeErro.style.display = "inline"
+  })
+}
+})
